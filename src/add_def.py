9fc2c9ae877e8d310cdbf8694b5acdeb254f55a1
lam <lthiet@student.ethz.ch>
2020-10-22 13:08:55 +0200

    fetch def

--- src/add_def.py
+++ src/add_def.py
@@ -2,14 +2,23 @@ import requests
 import json
 import sys
 
-with open('../data/credentials.json', 'r') as f:
-    cred = json.load(f)
+# with open('../data/credentials.json', 'r') as f:
+#     cred = json.load(f)
 
-language = "en-gb"
-word_id = "example"
-url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + \
-    language + "/" + word_id.lower()
-r = requests.get(url, headers=cred)
+# language = "en-gb"
+# word_id = "superfluous"
+# url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + \
+#     language + "/" + word_id.lower()
+# r = requests.get(url, headers=cred)
 
-print("code {}\n".format(r.status_code))
-print("json \n" + json.dumps(r.json()))
+# with open('../data/test.json', 'w') as f:
+#     json.dump(r.json(), f, indent=4)
+
+with open('../data/test.json', 'r') as f:
+    data = json.load(f)
+
+
+def fetch_definition(entry):
+    # TODO: look into the documentation of Oxford API to understand the
+    # json structure
+    return entry["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"]
