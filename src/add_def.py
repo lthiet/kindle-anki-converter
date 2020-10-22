1698738705198caca95f9825dea237e9f11e0aa7
lam <lthiet@student.ethz.ch>
2020-10-22 16:42:22 +0200

    fetch def

--- src/add_def.py
+++ src/add_def.py
@@ -2,23 +2,44 @@ import requests
 import json
 import sys
 
-# with open('../data/credentials.json', 'r') as f:
-#     cred = json.load(f)
+# Fetch credentials
+with open('../data/credentials.json', 'r') as f:
+    cred = json.load(f)
 
-# language = "en-gb"
-# word_id = "superfluous"
-# url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + \
-#     language + "/" + word_id.lower()
-# r = requests.get(url, headers=cred)
-
-# with open('../data/test.json', 'w') as f:
-#     json.dump(r.json(), f, indent=4)
-
-with open('../data/test.json', 'r') as f:
-    data = json.load(f)
+# Default language
+language = "en-gb"
 
 
-def fetch_definition(entry):
-    # TODO: look into the documentation of Oxford API to understand the
-    # json structure
-    return entry["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"]
+def fetch_definition(word):
+    """
+    Contact the Oxford Dictionary API and fetch a definition
+    TODO: could be improved if passed a list of word?
+    """
+    url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + \
+        language + "/" + word.lower()
+    result = requests.get(url, headers=cred).json()
+
+    # Check if the API has returned an error
+    try:
+        result['error']
+        return []
+    except KeyError:
+        # TODO: look into the documentation of Oxford API to understand the
+        # json structure
+        return result["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"]
+
+
+# Read the words for which we will fetch the definition
+with open('../data/vocab.json', 'r') as f:
+    vocab = json.load(f)
+
+
+# Populate the definitions list
+definitions = []
+for word in vocab["stems"]:
+    definitions.append(fetch_definition(word))
+vocab["definitions"] = definitions
+
+# Write to disk
+with open('../data/vocab.json', 'w') as f:
+    json.dump(vocab, f, indent=4)
