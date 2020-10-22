6a85ebc4b2efa3ac95ca0a5f8346e62638f50d68
lam <lthiet@student.ethz.ch>
2020-10-22 12:58:12 +0200

    add def wip

--- src/add_def.py
+++ src/add_def.py
@@ -2,5 +2,14 @@ import requests
 import json
 import sys
 
-cred = json.load("../data/credentials.json")
-print(cred)
+with open('../data/credentials.json', 'r') as f:
+    cred = json.load(f)
+
+language = "en-gb"
+word_id = "example"
+url = "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + \
+    language + "/" + word_id.lower()
+r = requests.get(url, headers=cred)
+
+print("code {}\n".format(r.status_code))
+print("json \n" + json.dumps(r.json()))
