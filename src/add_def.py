5aa613c875c6bd79df35822358c12936cf3f08b8
lam <lthiet@student.ethz.ch>
2020-10-22 12:50:16 +0200

    convert db to json

---
+++ src/add_def.py
@@ -0,0 +1,5 @@
+import requests
+import json
+import sys
+
+app_id = sys.arg
---
+++ src/create_credentials.py
@@ -0,0 +1,13 @@
+import json
+import sys
+
+app_id = sys.argv[1]
+app_key = sys.argv[2]
+
+obj = {
+    "app_id": app_id,
+    "app_key": app_key
+}
+
+with open("../data/credentials.json", "r") as f:
+    json.dump(obj, f, indent=4)
